25e18c8ad8100488996f292386059710968febef
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-02-28 21:53:39 +0300

    handling routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import CreateProduct from './components/CreateProduct';
+import ListProducts from './components/ListProducts';
 
 function App() {
   return (
     <div className="App">
      <h1>Scandiweb</h1>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Product List</Link>
+            </li>
+            <li>
+              <Link to="product/create">Create Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<ListProducts />} />
+          <Route path="product/create" element={<CreateProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/ListProducts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListProducts() {
+  return (
+    <h1>ListProducts</h1>
+  )
+}
+
+export default ListProducts;
