56ef3e2f7f9ab43d76609cbe73d14849e2d06f1c
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-03-01 22:59:22 +0300

    axios i

--- client/src/components/CreateProduct.js
+++ client/src/components/CreateProduct.js
@@ -10,7 +10,7 @@ function CreateProduct() {
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({values, [name]: value}));
+    setInputs(values => ({...values, [name]: value}));
 
   }
 
